9a7828146a91da9585406463801d4d6f98406f35
Babacar Mbaye <papemb331@gmail.com>
2025-11-26 19:31:14 +0000

    reoganise fichier

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php require './pages/header.php' ?>
-
-<h1>Bonjour tout monde !</h1>
-
-<?php require './pages/footer.php' ?>
---
+++ pages/index.php
@@ -0,0 +1,5 @@
+<?php require '../elements/header.php' ?>
+
+<h1>Bonjour tout monde !</h1>
+
+<?php require '../elements/footer.php' ?>
